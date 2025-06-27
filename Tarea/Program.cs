a7473963e823deb7f0e11f55c5a678aef51463da
Claribel <claridiaz02@gmail.com>
2025-06-27 15:58:00 -0300

    Se sube el ejercicio 1

---
+++ Tarea/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+class Program{
+private static readonly HttpClient client = new HttpClient();
+
+    static async Task Main(string[] args)
+    {
+    await GetTareas();
+    
+}
+private static async Task GetTareas()
+{
+    var url = "https://jsonplaceholder.typicode.com/todos/";
+    var response = await client.GetAsync(url);
+    response.EnsureSuccessStatusCode();
+    var responseBody = await response.Content.ReadAsStringAsync();
+
+    List<Tarea> tareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+
+    foreach (var tarea in tareas)
+    {
+        string estado = tarea.Completo ? "Completada" : "Pendiente";
+        Console.WriteLine($"- {tarea.Titulo} [{estado}]");
+    }
+
+    // Guardar todas las tareas en un archivo JSON
+    string jsonCompleto = JsonSerializer.Serialize(tareas, new JsonSerializerOptions { WriteIndented = true });
+    await File.WriteAllTextAsync("tareas.json", jsonCompleto);
+    Console.WriteLine("\nArchivo tareas.json guardado con éxito!");
+}
+}
+
---
+++ Tarea/Tarea.cs
@@ -0,0 +1,13 @@
+
+public class Tarea
+{
+    private int usuarioId;
+    private int id;
+    private string titulo;
+    private bool completo;
+
+    public int UsuarioId { get; set ; }
+    public int Id { get; set ; }
+    public string Titulo { get ; set ; }
+    public bool Completo { get ; set; }
+}
